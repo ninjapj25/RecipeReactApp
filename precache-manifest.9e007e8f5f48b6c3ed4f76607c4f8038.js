ce994dfbfc4ccc048cabf6af95259b5174c3f5f0
ninjapj25 <ninjapj25@gmail.com>
2020-09-20 20:18:23 +0800

    Updates

---
+++ precache-manifest.9e007e8f5f48b6c3ed4f76607c4f8038.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1bd552abbc998700a854c65f5513d9",
+    "url": "/RecipeReactApp/index.html"
+  },
+  {
+    "revision": "0527d6b6a025fa4d1a9f",
+    "url": "/RecipeReactApp/static/css/main.f19bacaa.chunk.css"
+  },
+  {
+    "revision": "ed5c78a1fb990fcfe6fd",
+    "url": "/RecipeReactApp/static/js/2.249f6b21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeReactApp/static/js/2.249f6b21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0527d6b6a025fa4d1a9f",
+    "url": "/RecipeReactApp/static/js/main.9d409866.chunk.js"
+  },
+  {
+    "revision": "6b909c58e4398e941593",
+    "url": "/RecipeReactApp/static/js/runtime-main.cefb2e2e.js"
+  },
+  {
+    "revision": "769e40e08669133205b5f1dce39e8f31",
+    "url": "/RecipeReactApp/static/media/Logo.769e40e0.svg"
+  },
+  {
+    "revision": "fcfab8633a22e008c7682ebbaab28c39",
+    "url": "/RecipeReactApp/static/media/Search.fcfab863.svg"
+  }
+]);
